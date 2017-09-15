96baf04fa49e664fb4af06c411a2cf0c5d849cd6
Nightly <nightly@outlook.kr>
2017-09-16 02:17:39 +0900

    .

---
+++ start.cmd
@@ -0,0 +1,13 @@
+CD %~dp0
+#Install RDP Wrapper
+START /B /D bin\RDPWrap-v1.6.1 RDPWInst.exe -i -o
+
+#Change Group Policy Auto Update system restart
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 3 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallDay /t REG_DWORD /d 0 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallTime /t REG_DWORD /d 3 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
+
+#gpupdate /force
+
