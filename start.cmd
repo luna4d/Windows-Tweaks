b3b3d2282a36a7ae88280c77574a1d58f0feead6
Nightly <nightly@outlook.kr>
2018-03-11 08:39:36 +0900

    .

--- start.cmd
+++ start.cmd
@@ -1,33 +1,37 @@
 CD %~dp0
 
-#Install RDP Wrapper
+REM Install RDP Wrapper
 START /B /D bin\RDPWrap-v1.6.1 RDPWInst.exe -i -o
 
-#Change Option Control Panel
+REM Change Option Control Panel
 REG ADD HKCU\Control Panel\Mouse /v DoubleClickSpeed /t REG_SZ /d "100" /f
 REG ADD HKCU\Control Panel\Keyboard /v KeyboardDelay /t REG_SZ /d "0" /f
 REG ADD HKCU\Control Panel\Keyboard /v KeyboardSpeed /t REG_SZ /d "31" /f
 
-#HDD saving
+REM HDD saving
 powercfg /setacvalueindex SCHEME_CURRENT 381b4222-f694-41f0-9685-ff5bb260df2e 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
-#PCIe SSD if unresponsive and requires a power cycle. (not for NVME)
+REM PCIe SSD if unresponsive and requires a power cycle. (not for NVME)
 powercfg /setacvalueindex SCHEME_CURRENT 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0
 
-#Change Group Policy Auto Update system restart
+REM Change Group Policy Auto Update system restart
 REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 3 /f
 REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f
 REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallDay /t REG_DWORD /d 0 /f
 REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v ScheduledInstallTime /t REG_DWORD /d 3 /f
 REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoRebootWithLoggedOnUsers /t REG_DWORD /d 1 /f
 
-#gpupdate /force
+REM gpupdate /force
 
-# Theme optimize
+REM Theme optimize
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects /v VisualFXSetting /t REG_DWORD /d 3 /f
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarAnimations /t REG_DWORD /d 0 /f
 REG ADD "HKCU\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_SZ /d "0" /f
 REG ADD "HKCU\Control Panel\Desktop" /v FontSmoothing /t REG_SZ /d "0" /f
 REG ADD "HKCU\Control Panel\Desktop" /v UserPreferencesMask /t REG_BINARY /d "9012028010000000" /f
 
-# default 2  38:FORGROUND 24:BACKGROUND 18:BALANCE
+REM default 2  38:FORGROUND 24:BACKGROUND 18:BALANCE
 REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\PriorityControl /v Win32PrioritySeparation /t REG_DWORD /d 18 /f
+
+REM Service disable
+sc config lfsvc start=disabled
+sc config DiagTrack start=disabled
