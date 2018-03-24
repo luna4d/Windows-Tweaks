597e214c1d1ecea38ca8bc59f0ff6b860c7cbf55
Nightly <nightly@outlook.kr>
2018-03-25 07:06:40 +0900

    .

--- start.cmd
+++ start.cmd
@@ -8,10 +8,14 @@ REG ADD "HKCU\Control Panel\Mouse" /v DoubleClickSpeed /t REG_SZ /d "100" /f
 REG ADD "HKCU\Control Panel\Keyboard" /v KeyboardDelay /t REG_SZ /d "0" /f
 REG ADD "HKCU\Control Panel\Keyboard" /v KeyboardSpeed /t REG_SZ /d "31" /f
 
+REM SLEEP OFF
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 381b4222-f694-41f0-9685-ff5bb260df2e 238c9fa8-0aad-41ed-83f4-97be242c8f20 29f6c1db-86da-48c5-9fdb-f2b67b1f44da 0
+REM HIBERNATE OFF
+powercfg /HIBERNATE OFF
 REM HDD saving
-powercfg /setacvalueindex SCHEME_CURRENT 381b4222-f694-41f0-9685-ff5bb260df2e 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 381b4222-f694-41f0-9685-ff5bb260df2e 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
 REM PCIe SSD if unresponsive and requires a power cycle. (not for NVME)
-powercfg /setacvalueindex SCHEME_CURRENT 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 381b4222-f694-41f0-9685-ff5bb260df2e 501a4d13-42af-4429-9fd1-a8218c268e20 ee12f906-d277-404b-b6da-e5fa1a576df5 0
 
 REM Change Group Policy Auto Update system restart
 REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 3 /f
